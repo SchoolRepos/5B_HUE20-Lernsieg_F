105de4a8311d75f7b6d94cbeb350c08c73464345
Fabian Gurtner <fabian.paul.gurtner@gmail.com>
2023-05-07 22:37:53 +0200

    Implement login guard

--- lernsieg-frontend/src/app/app-routing.module.ts
+++ lernsieg-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {LoginComponent} from "./login/login.component";
 import {AdminPanelComponent} from "./admin-panel/admin-panel.component";
+import {LoginGuard} from "./login.guard";
 
 const routes: Routes = [
     {path: "login", component: LoginComponent},
-    {path: "admin-panel", component: AdminPanelComponent}
+    {path: "admin-panel", component: AdminPanelComponent, canActivate: [LoginGuard]}
 ];
 
 @NgModule({
